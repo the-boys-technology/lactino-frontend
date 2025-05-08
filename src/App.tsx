37bf4e76dc038624f2557168f32a2dfd49638624
Pedro Mota <110642021+mota-pedro@users.noreply.github.com>
2025-05-08 00:06:40 -0300

    feat: dashboard inicial com componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import './css/mainpage.css'
+import Header from './components/Header'
+import Dashboard from './components/Dashboard'
 
 function App() {
-  return <h1>Hello, React + TypeScript!</h1>
+  return (
+    <>
+    <Header />
+      <main className="app__main">
+        <Dashboard/>
+      </main>
+    </>
+  )
 }
 
 export default App
