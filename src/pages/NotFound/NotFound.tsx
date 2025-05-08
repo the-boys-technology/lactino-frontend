46034580c0dc5d1734241c0a366ab41b5dbd08c5
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2025-05-08 00:39:22 -0300

    feat: NotFound Page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <h1>Página não encontrada</h1>
+        <p>Erro 404 – Verifique a URL digitada.</p>
+      </div>
+    );
+  }
