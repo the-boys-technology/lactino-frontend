8aafb8591f06c94600bc75ed4b3d08d2d7e68029
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2025-05-12 13:24:09 -0300

    feat: criando type para Compras

---
+++ src/types/Compras.ts
@@ -0,0 +1,12 @@
+export interface Compra {
+  tipoInsumo: string;
+  produto: string;
+  dataCompra: string;
+  formaPagamento: string;
+  quantidade: string;
+  valorUnitario: string;
+  valorTotal: string;
+  validadeProduto: string;
+  fornecedor: string;
+  observacao: string;
+}
