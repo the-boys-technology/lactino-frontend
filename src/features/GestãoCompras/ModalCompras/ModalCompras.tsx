6422c1e4963509c4942123dfd249ce6244830631
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2025-05-12 13:36:36 -0300

    feat: integrando componentes existentes no modal

--- src/features/GestãoCompras/ModalCompras/ModalCompras.tsx
+++ src/features/GestãoCompras/ModalCompras/ModalCompras.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Campo from "../../../components/Campo";
+import Botao from "../../../components/Botao";
 import "../ModalCompras/ModalCompras.css";
 
 interface Props {
@@ -31,47 +33,38 @@ export default function ModalCompra({ onClose, onSave }: Props) {
 
   return (
     <div className="modal">
-      <h2 className="modal__titulo">Registrar Compras</h2>
+      <div className="modal__container">
+        <h2 className="modal__titulo">Registrar Compras</h2>
 
-      <div className="modal__form">
-        <div className="modal__coluna">
-          <label>Fornecedor:</label>
-          <input name="fornecedor" value={form.fornecedor} onChange={handleChange} />
-
-          <label>Tipo de Insumo:</label>
-          <input name="tipoInsumo" value={form.tipoInsumo} onChange={handleChange} />
-
-          <label>Produto:</label>
-          <input name="produto" value={form.produto} onChange={handleChange} />
-
-          <label>Data da Compra:</label>
-          <input name="dataCompra" value={form.dataCompra} onChange={handleChange} />
-
-          <label>Forma de Pagamento:</label>
-          <input name="formaPagamento" value={form.formaPagamento} onChange={handleChange} />
+        <div className="modal__form">
+          <div className="modal__coluna">
+            <Campo name="fornecedor" placeholder="Fornecedor" type="text" onChange={handleChange} />
+            <Campo name="tipoInsumo" placeholder="Tipo de Insumo" type="text" onChange={handleChange} />
+            <Campo name="produto" placeholder="Produto" type="text" onChange={handleChange} />
+            <Campo name="dataCompra" placeholder="Data da Compra" type="date" onChange={handleChange} />
+            <Campo name="formaPagamento" placeholder="Forma de Pagamento" type="text" onChange={handleChange} />
+          </div>
+          <div className="modal__coluna">
+            <Campo name="quantidade" placeholder="Quantidade" type="text" onChange={handleChange} />
+            <Campo name="valorUnitario" placeholder="Valor Unitário" type="text" onChange={handleChange} />
+            <Campo name="valorTotal" placeholder="Valor Total" type="text" onChange={handleChange} />
+            <Campo name="validadeProduto" placeholder="Validade do Produto" type="date" onChange={handleChange} />
+            <section className="campo-container">
+              <h4 className="campo-container__nome">Observação:</h4>
+              <textarea
+                name="observacao"
+                className="campo-container__input"
+                rows={3}
+                onChange={handleChange}
+              />
+            </section>
+          </div>
         </div>
 
-        <div className="modal__coluna">
-          <label>Quantidade:</label>
-          <input name="quantidade" value={form.quantidade} onChange={handleChange} />
-
-          <label>Valor Unitário:</label>
-          <input name="valorUnitario" value={form.valorUnitario} onChange={handleChange} />
-
-          <label>Valor Total:</label>
-          <input name="valorTotal" value={form.valorTotal} onChange={handleChange} />
-
-          <label>Validade do Produto:</label>
-          <input name="validadeProduto" value={form.validadeProduto} onChange={handleChange} />
-
-          <label>Observação:</label>
-          <textarea name="observacao" value={form.observacao} onChange={handleChange} />
+        <div className="modal__botoes">
+          <Botao tipo="secondary" label="Cancelar" onClick={onClose} />
+          <Botao tipo="primary" label="Salvar" onClick={handleSubmit} />
         </div>
-      </div>
-
-      <div className="modal__botoes">
-        <button className="modal__botao cancelar" onClick={onClose}>Cancelar</button>
-        <button className="modal__botao salvar" onClick={handleSubmit}>Salvar</button>
       </div>
     </div>
   );
