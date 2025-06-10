59409412765220cce82eb24f48348182d078945c
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2025-06-09 23:15:41 -0300

    feat: Componentizando Tabela de Transações

---
+++ src/components/TabelaTransacoes.tsx
@@ -0,0 +1,55 @@
+import "../css/tabela-trasacoes.css";
+import { Cliente, Fornecedor, Transacao } from "../types/transacao";
+import { formatarData, formatarDinheiro } from "../utils/formatter_utils";
+
+interface TabelaTransacoesProps {
+  transacoes: Transacao[];
+  clientes?: Cliente[]; // se for VENDA
+  fornecedores?: Fornecedor[]; // se for COMPRA
+  onVerRelatorio: (transacao: Transacao) => void;
+  tipoTransacao: "VENDA" | "COMPRA";
+}
+
+export default function TabelaTransacoes({
+  transacoes,
+  clientes = [],
+  fornecedores = [],
+  onVerRelatorio,
+  tipoTransacao
+}: TabelaTransacoesProps) {
+  return (
+    <section className="tabela-transacoes">
+      <div className="tabela-transacoes__cabecalho">
+        <span>{tipoTransacao === "VENDA" ? "Cliente" : "Fornecedor"}</span>
+        <span>Produtos</span>
+        <span>Data da compra</span>
+        <span>Preço Total</span>
+        <span>Pagamento</span>
+        <span>Ações</span>
+      </div>
+
+      {transacoes.map((item) => {
+        const nomePessoa =
+          tipoTransacao === "VENDA"
+            ? clientes.find((c) => c.id === item.clienteId)?.nome
+            : fornecedores.find((f) => f.id === item.fornecedorId)?.nome;
+
+        return (
+          <div key={item.id} className="tabela-transacoes__linha">
+            <span>{nomePessoa || "—"}</span>
+            <span>{item.itens.length}</span>
+            <span>{formatarData(item.data)}</span>
+            <span>{formatarDinheiro(item.valorTotal)}</span>
+            <span>{item.formaPagamento}</span>
+            <button
+              className="tabela-transacoes__ver-relatorio"
+              onClick={() => onVerRelatorio(item)}
+            >
+              🔍 Ver Relatório
+            </button>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
